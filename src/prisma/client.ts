a829385f2dffb04934295454a0f5a60af02445a8
pedromota05 <pedro.hmota.goncalves@gmail.com>
2026-05-28 18:50:13 -0400

    fix: implement official postgres adapter for prisma 7

--- src/prisma/client.ts
+++ src/prisma/client.ts
@@ -1,3 +1,13 @@
-import { PrismaClient } from "../../generated/prisma/client";
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../generated/prisma/client';
 
-export const prisma = new PrismaClient({} as any);
+// Puxa a URL de conexão do Render (ou do seu .env local)
+const connectionString = process.env.DATABASE_URL;
+
+// Cria o motor de conexão do Postgres
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+// Entrega o motor para o Prisma
+export const prisma = new PrismaClient({ adapter });
